e17b838f893917e393086325b58597e1d7489c0a
Sam Uherek <samuherekbiz@gmail.com>
2024-02-08 17:50:23 +0100

    getting and printing the different sections of sudoku

--- src/main.rs
+++ src/main.rs
@@ -104,35 +104,104 @@ impl fmt::Display for Board {
     }
 }
 
-fn print_col(grid: &Vec<u8>, idx: u8) {
-    for row in 0..9 {
-        let coord = row * 9 + idx;
-        println!("{}", grid[coord as usize]);
-    }
+const SDK_SIZE: usize = 9;
+
+type ColIdx = u8;
+type RowIdx = u8;
+type SqrIdx = u8;
+type Grid = Vec<Cell>;
+type GridCol = [Cell; SDK_SIZE];
+type GridRow = [Cell; SDK_SIZE];
+type GridSqr = [Cell; SDK_SIZE];
+
+const SIZE_RANGE: Range<usize> = 0..SDK_SIZE;
+
+fn get_cell(grid: &Grid, idx: usize) -> Cell {
+    grid.get(idx).and_then(|x| x.clone())
 }
 
-fn print_row(grid: &Vec<u8>, idx: u8) {
-    for col in 0..9 {
-        let coord = idx * 9 + col;
-        print!("{}", grid[coord as usize]);
-    }
-    println!("");
-}
-
-fn print_square(grid: &Vec<u8>, idx: u8) {
-    let start_row = (idx / 3) * 3;
-    let start_col = (idx % 3 ) * 3;
-
-    for row_idx in start_row..(start_row + 3) {
-        for col_idx in start_col..(start_col + 3) {
-            let coord = row_idx * 9 + col_idx;
-            print!("{}", grid[coord as usize]);
+fn get_col(grid: &Grid, idx: ColIdx) -> GridCol {
+    let mut cells = [None; SDK_SIZE];
+    if SIZE_RANGE.contains(&idx.into()) {
+        for row_idx in 0..SDK_SIZE {
+           let coord = row_idx * SDK_SIZE + idx as usize;
+           cells[row_idx] = get_cell(grid, coord);
         }
-        print!("\n");
+    }
+    cells
+}
+
+fn get_row(grid: &Grid, idx: RowIdx) -> GridRow {
+    let mut cells = [None; SDK_SIZE];
+    let row_start = (idx as usize) * SDK_SIZE;
+    if SIZE_RANGE.contains(&idx.into()) {
+        for col_idx in 0..SDK_SIZE {
+            let coord = row_start + col_idx;
+            cells[col_idx] = get_cell(grid, coord);
+        }
+    }
+    cells
+}
+
+fn get_square(grid: &Grid, idx: SqrIdx) -> GridSqr {
+    let mut cells = [None; SDK_SIZE];
+    let row_start = (idx as usize) / 3 * 3;
+    let col_start = (idx as usize) % 3 * 3;
+
+    if SIZE_RANGE.contains(&idx.into()) {
+        let mut cell_idx = 0;
+        for row_idx in row_start..(row_start + 3) {
+            for col_idx in col_start..(col_start + 3) {
+                let coord = row_idx * 9 + col_idx;
+                cells[cell_idx] = get_cell(grid, coord);
+                cell_idx += 1;
+            }
+        }
+    }
+    cells
+}
+
+fn get_cell_char(val: &Cell) -> char {
+    match val {
+        Some(v) => (v + 48) as char,
+        None => '_',
     }
 }
 
-fn print_grid(grid: &Vec<u8>) {
+fn print_cell(val: &Cell) {
+    let ch = get_cell_char(val);
+    println!("{}", ch);
+}
+
+fn print_col(grid: &Grid, idx: ColIdx) {
+    let mut value = String::new();
+    for item in get_col(grid, idx) {
+        value.push(get_cell_char(&item));
+        value.push('\n');
+    }
+    println!("{value}");
+}
+
+fn print_row(grid: &Grid, idx: RowIdx) {
+    let mut value = String::new();
+    for item in get_row(grid, idx) {
+        value.push(get_cell_char(&item));
+    }
+    println!("{value}");
+}
+
+fn print_sqr(grid: &Grid, idx: u8) {
+    let mut value = String::new();
+    for (i, item) in get_square(grid, idx).iter().enumerate() {
+        value.push(get_cell_char(&item));
+        if i % 3 == 2 {
+            value.push('\n');
+        }
+    }
+    println!("{value}");
+}
+
+fn print_grid(grid: &Grid) {
     let mut value = String::new();
 
     for (idx, x) in grid.iter().enumerate() {
@@ -143,7 +212,7 @@ fn print_grid(grid: &Vec<u8>) {
             value.push('|');
         }
 
-        value.push(char::from_digit(*x as u32, 10).unwrap());
+        value.push(get_cell_char(x));
         value.push('|');
 
         if is_box_boundary {
@@ -169,7 +238,7 @@ fn print_grid(grid: &Vec<u8>) {
 //     true
 // }
 
-pub fn new_grid() -> Vec<u8> {
+pub fn new_grid() -> Grid {
     let mut grid = Vec::new(); 
     // let mut available: Vec<u8> = (1..=9).collect();
     let mut cursor = 0;
@@ -198,7 +267,7 @@ fn main() {
 
     let grid = new_grid();
     print_grid(&grid);
-    print_square(&grid, 1);
+    print_sqr(&grid, 1);
     print_row(&grid, 0);
     print_col(&grid, 0);
 }
